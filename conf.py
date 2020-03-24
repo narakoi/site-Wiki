4fdb5cb93a7cf8f1b8ec41911e0a5be44bf1bae1
narakoi <vladrabokurakura@gmail.com>
2020-03-25 05:05:08 +0800

    c

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "https://wiki.imalan.cn/"
+site_prefix = "https://wiki.koi.cat/"
 source_dir = "../src/"
 build_dir = "../dist/"
 template = {
@@ -15,20 +15,20 @@ index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "AlanDecode/site-Wiki@gh-pages"
+    "repo": "narakoi/wiki@gh-pages"
 }
 category_by_folder = True
 for_manual_build_trigger = 1
 
 # 站点设置
-site_name = "無知識 | 三無計劃"
-site_logo = "${static_prefix}android-chrome-512x512.png"
+site_name = "Escape Wiki"
+site_logo = "${static_prefix}logo-头像.jpg"
 site_build_date = "2017-06-29T12:00+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "熊猫小A的Wiki站点"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'wiki']
+author = "koi"
+email = "narakyzlily@gmail.com"
+author_homepage = "https://koi.cat"
+description = "koi wiki"
+key_words = ['Maverick', 'koi', 'Galileo', 'wiki']
 language = 'zh-CN'
 
 valine = {
@@ -43,29 +43,14 @@ valine = {
 
 external_links = [
     {
-        "name": "TRIPLE NULL",
-        "url": "https://www.imalan.cn",
+        "name": "wiki",
+        "url": "https://wiki.koi.cat",
         "brief": "三是虚指。至于是哪三无，我唔知。"
     },
     {
         "name": "BLOG",
-        "url": "https://blog.imalan.cn",
+        "url": "https://koi.cat",
         "brief": "熊猫小A的博客。隶属于「三无计划」。"
-    },
-    {
-        "name": "LAB",
-        "url": "https://lab.imalan.cn",
-        "brief": "熊猫小A的实验室。隶属于「三无计划」。"
-    },
-    {
-        "name": "GITHUB",
-        "url": "https://github.com/AlanDecode",
-        "brief": "My GitHub"
-    },
-    {
-        "name": "CHANNEL",
-        "url": "https://t.me/triple_null",
-        "brief": "熊猫小A的广播。隶属于「三无计划」。"
     }
 ]
 nav = [
@@ -125,8 +110,8 @@ head_addon = r'''
 '''
 
 footer_addon = r'''
-<a no-style href="http://beian.miit.gov.cn" target="_blank">京ICP备18000133号-1</a> | 
-<a no-style href="https://www.upyun.com" target="_blank">又拍云</a>
+<a no-style href="http://koi.cat" target="_blank">KOI</a> | 
+<a no-style href="https://kois.pw" target="_blank">koicloud</a>
 '''
 
 body_addon = r'''
